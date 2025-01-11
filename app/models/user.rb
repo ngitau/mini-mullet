d3e00e6e7f6db5e83fd16dddfce5a7cd3d40f5a2
Joseph G. <joseph@ndungugitau.com>
2025-01-11 09:56:10 +0300

    Adds secure_password, validations and normalizations to `User`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 10, maximum: 16 }
+
+  normalizes :name, with: ->(value) { value.strip }
 end
