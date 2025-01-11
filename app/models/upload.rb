79d6d6233480ed384fd8aaf51b23a9552eb261e9
Joseph G. <joseph@ndungugitau.com>
2025-01-11 20:37:27 +0300

    Removes duplication when handling  `CSV::MalformedCSVError` exception

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -12,11 +12,17 @@ class Upload
     @csv ||= begin
                CSV.parse(file&.read || "", headers: true)
              rescue CSV::MalformedCSVError
-               errors.add(:file, I18n.t("activemodel.errors.models.upload.attributes.file.must_be_valid_csv_file"))
-               false
+               handle_malformed_csv_error
              end
   end
 
   def process_in_batches(batch_size = 100)
+  rescue CSV::MalformedCSVError
+    handle_malformed_csv_error
+  end
+
+  def handle_malformed_csv_error
+    errors.add(:file, I18n.t("activemodel.errors.models.upload.attributes.file.must_be_valid_csv_file"))
+    false
   end
 end
