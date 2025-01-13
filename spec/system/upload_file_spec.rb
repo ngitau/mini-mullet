4382667b948c2615c71ac4ffd15faa1f220846a2
Joseph G. <joseph@ndungugitau.com>
2025-01-13 19:07:18 +0300

    Update upload_file_spec.rb

--- spec/system/upload_file_spec.rb
+++ spec/system/upload_file_spec.rb
@@ -1,8 +1,10 @@
-describe "upload file" do
-  context 'when loaded' do
-    it "displays the upload file form" do
+describe "upload file page" do
+  context 'loaded' do
+    before do
       visit new_upload_path
+    end
 
+    it "displays the upload file form" do
       expect(page).to have_selector('input[type="file"]')
       expect(page).to have_selector('input[type="submit"]')
 
@@ -15,12 +17,32 @@ describe "upload file" do
       expect(page).to have_selector('[data-action="click->upload#upload"]')
     end
 
-    context "when a file is uploaded" do
-      skip
-    end
+    context 'when form is submitted' do
+      context "with no file was chosen" do
+        it "notifies the user" do
+          click_button 'Upload'
 
-    context "when an invalid file is uploaded" do
-      skip
+          expect(page).to have_content 'No file selected.'
+        end
+      end
+
+      context "when a file is provided" do
+        context "with a valid file" do
+          it "uploads the file and shows success message" do
+            skip "AJAX request not getting triggered"
+
+            attach_file 'upload[file]', Rails.root.join("spec", "fixtures", "valid_file.csv")
+
+            click_button 'Upload'
+
+            expect(page).to have_content 'The file was processed successfully.'
+          end
+        end
+
+        context "with an invalid file" do
+          skip "AJAX request not getting triggered"
+        end
+      end
     end
   end
 end
