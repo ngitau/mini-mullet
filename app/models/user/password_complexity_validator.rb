990dc4ca66e225ef87cd218724c92a1aefc0a893
Joseph G. <joseph@ndungugitau.com>
2025-01-13 20:50:58 +0300

    Improves readability

--- app/models/user/password_complexity_validator.rb
+++ app/models/user/password_complexity_validator.rb
@@ -15,25 +15,25 @@ class User::PasswordComplexityValidator < ActiveModel::Validator
   private
 
   def validate_has_digit(record)
-    return if password =~ /\d/
+    return if /\d/.match?(password)
 
     record.errors.add(:password, I18n.t("activerecord.errors.models.user.attributes.password.must_have_digit"))
   end
 
   def validate_has_lowercase_char(record)
-    return if password =~ /[a-z]/
+    return if /[a-z]/.match?(password)
 
     record.errors.add(:password, I18n.t("activerecord.errors.models.user.attributes.password.must_have_lowercase_char"))
   end
 
   def validate_has_uppercase_char(record)
-    return if password =~ /[A-Z]/
+    return if /[A-Z]/.match?(password)
 
     record.errors.add(:password, I18n.t("activerecord.errors.models.user.attributes.password.must_have_uppercase_char"))
   end
 
   def validate_has_three_repeating_char(record)
-    if password =~ /(.)\1\1/
+    if /(.)\1\1/.match?(password)
       record.errors.add(:password, I18n.t("activerecord.errors.models.user.attributes.password.must_not_have_3_repeating_chars"))
     end
   end
