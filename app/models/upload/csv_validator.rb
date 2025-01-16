fa44c8430e48198a9ef0016648c8385ad1f3a80b
Joseph G. <joseph@ndungugitau.com>
2025-01-17 00:59:09 +0300

    Validates uploaded file is less than 10MB

--- app/models/upload/csv_validator.rb
+++ app/models/upload/csv_validator.rb
@@ -1,10 +1,16 @@
 class Upload::CsvValidator < ActiveModel::Validator
-  attr_reader :csv
+  attr_reader :file, :csv
 
   def validate(record)
+    @file = record.file
     @csv = record.csv
 
     validate_rows_present(record)
+
+    if file.present?
+      validate_size(record)
+    end
+
     validate_headers_present(record)
   end
 
@@ -14,6 +20,12 @@ class Upload::CsvValidator < ActiveModel::Validator
     return if csv.count > 0
 
     record.errors.add(:file, I18n.t("activemodel.errors.models.upload.attributes.file.must_have_data_rows"))
+  end
+
+  def validate_size(record)
+    return if file && File.size(file.path) < 10*(1024**2)
+
+    record.errors.add(:file, I18n.t("activemodel.errors.models.upload.attributes.file.must_be_within_size_limit"))
   end
 
   def validate_headers_present(record)
